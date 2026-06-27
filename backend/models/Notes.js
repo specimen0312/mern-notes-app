7370214553c72ac493c0681a95029ea524f4d941
specimen0312 <akbodkhe03@gmail.com>
2026-06-27 10:52:08 -0700

    Add note model

---
+++ backend/models/Notes.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,   
+        required: true,
+        trim: true
+    },
+    content: {
+        type: String,
+        required: true,
+        trim: true  
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+}, { timestamps: true });       
+
+const Note = mongoose.model("Note", noteSchema);
+
+export default Note;
